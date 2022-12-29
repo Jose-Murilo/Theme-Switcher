a5c3a37ec5ee5cda9f2874eca8e8fffd9d409187
Jose Murilo <murilojucas360@hotmail.com>
2022-12-29 14:22:21 -0300

    add script more clean

--- script.js
+++ script.js
@@ -4,50 +4,24 @@ const imgSun = document.querySelector('.img-sun')
 const header = document.querySelector('header')
 const footer = document.querySelector('footer')
 
-ball.addEventListener('click', () => {
-    
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark')        
-        header.style.color = 'black'
-        footer.style.color = 'black'
-        ball.style.transform = ''
-        ball.style.transform = 'translate(0)'
-    } else {
-        document.body.classList.add('dark')
-        ball.style.transform = 'translate(-13.6rem)'
-        header.style.color = 'white'
-        footer.style.color = 'white'
-    }
-})
+function eventClick(nameProperty) {
+    nameProperty.addEventListener('click', () => {
 
-imgSun.addEventListener('click', () => {
-    
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark')        
-        header.style.color = 'black'
-        footer.style.color = 'black'
-        ball.style.transform = ''
-        ball.style.transform = 'translate(0)'
-    } else {
-        document.body.classList.add('dark')
-        ball.style.transform = 'translate(-13.6rem)'
-        header.style.color = 'white'
-        footer.style.color = 'white'
-    }
-})
+        if (document.body.classList.contains('dark')) {
+            document.body.classList.remove('dark')        
+            header.style.color = 'black'
+            footer.style.color = 'black'
+            ball.style.transform = ''
+            ball.style.transform = 'translate(0)'
+        } else {
+            document.body.classList.add('dark')
+            ball.style.transform = 'translate(-13.6rem)'
+            header.style.color = 'white'
+            footer.style.color = 'white'
+        }
+    })
+}
 
-imgMoon.addEventListener('click', () => {
-    
-    if (document.body.classList.contains('dark')) {
-        document.body.classList.remove('dark')        
-        header.style.color = 'black'
-        footer.style.color = 'black'
-        ball.style.transform = ''
-        ball.style.transform = 'translate(0)'
-    } else {
-        document.body.classList.add('dark')
-        ball.style.transform = 'translate(-13.6rem)'
-        header.style.color = 'white'
-        footer.style.color = 'white'
-    }
-})
+eventClick(ball)
+eventClick(imgSun)
+eventClick(imgMoon)
